3ee7f73676013316125f9bab72eff98321f68558
Max Burstein <joesmoe@teamtol.com>
2012-03-05 19:29:36 -0500

    My Blog

---
+++ burstolio/urls.py
@@ -0,0 +1,19 @@
+from django.conf.urls import patterns, include, url
+
+# Uncomment the next two lines to enable the admin:
+from django.contrib import admin
+admin.autodiscover()
+
+urlpatterns = patterns('',
+    # Examples:
+    url(r'^', include('core.urls')),
+    #url(r'^login/$', 'django.contrib.auth.views.login', {'template_name': 'core/login.html'}, name='login'),
+    #url(r'^logout/$', 'django.contrib.auth.views.logout', {'next_page': '/'}, name='logout'),
+    #url(r'^register/$', 'core.views.register', name='register'),
+
+    # Uncomment the admin/doc line below to enable admin documentation:
+    url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    # Uncomment the next line to enable the admin:
+    url(r'^admin/', include(admin.site.urls)),
+)
---
+++ core/templatetags/tags.py
@@ -0,0 +1,26 @@
+from core.models import Comment, Tag
+
+from django import template
+
+register = template.Library()
+
+#May want to ditch this for a middleware that passes in the comments object so that I can do the manipulations in the actual template
+@register.simple_tag
+def recent_comments():
+    
+    comments = Comment.objects.select_related('entry').filter(deleted=False, spam=False).order_by('-id')[:3]
+    output = '<ul id="recent">'
+    for comment in comments:
+        if comment.website:
+            output += '<li><a href="' + comment.website + '">' + comment.name + '</a> - <a href="' + comment.entry.get_absolute_url() + '">' + comment.entry.title + '</a></li>'
+        elif comment.user:
+            output += '<li><a href="http://www.github.com/mburst">' + comment.user.get_full_name() + '</a> - <a href="' + comment.entry.get_absolute_url() + '">' + comment.entry.title + '</a></li>'
+        else:
+            output += '<li>' + comment.name + ' - <a href="' + comment.entry.get_absolute_url() + '">' + comment.entry.title + '</a></li>'
+    output += '</ul>'
+    return output
+    
+@register.simple_tag
+def tags():
+    tags = Tag.objects.order_by('?')[:10]
+    return tags
