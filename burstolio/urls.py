bd1d4d6af3ba2f466fdbd35fc49547e2a347fa44
Max Burstein <joesmoe@teamtol.com>
2012-03-05 20:50:11 -0500

    URLs update to serve static files

--- burstolio/urls.py
+++ burstolio/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import patterns, include, url
+import settings
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
@@ -17,3 +18,8 @@ urlpatterns = patterns('',
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
 )
+
+if not settings.DEBUG:
+    urlpatterns += patterns('',
+        (r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT}),
+    )
