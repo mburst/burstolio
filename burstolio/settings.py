a9d140e8ed4c8568dd52f5b4f96bb6d5af5e9224
Max Burstein <joesmoe@teamtol.com>
2012-03-05 21:15:37 -0500

    Serve admin static files

--- burstolio/settings.py
+++ burstolio/settings.py
@@ -69,6 +69,8 @@ STATIC_ROOT = os.path.join(SITE_ROOT, 'static')
 # Example: "http://media.lawrence.com/static/"
 STATIC_URL = '/static/'
 
+ADMIN_MEDIA_PREFIX = '/static/admin/'
+
 # Additional locations of static files
 STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
