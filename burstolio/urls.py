798ba560763559b2fd06ae6bc08b2c927567cebf
Max Burstein <joesmoe@teamtol.com>
2012-03-10 14:16:51 -0500

    removed static serve url

--- burstolio/urls.py
+++ burstolio/urls.py
@@ -18,8 +18,3 @@ urlpatterns = patterns('',
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
 )
-
-'''if not settings.DEBUG:
-    urlpatterns += patterns('',
-        (r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT}),
-    )'''
