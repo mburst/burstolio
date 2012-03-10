9e0469f1aefe98f2c48fcd71c9bbe4f9dae8f059
Max Burstein <joesmoe@teamtol.com>
2012-03-10 13:18:27 -0500

    fix for dynamic port

--- tornadows.py
+++ tornadows.py
@@ -2,6 +2,13 @@ import os, sys
 import django.core.handlers.wsgi
 from tornado import httpserver, ioloop, wsgi
 
+from optparse import OptionParser
+parser = OptionParser()
+
+parser.add_option('--port', dest='port')
+
+options, args = parser.parse_args()
+
 def runserver():
     app_dir = os.path.abspath(os.path.dirname(__file__))
     sys.path.append(os.path.dirname(app_dir))
@@ -10,7 +17,7 @@ def runserver():
 
     container = wsgi.WSGIContainer(application)
     server = httpserver.HTTPServer(container)
-    server.listen(29066)
+    server.listen(options.port)
     try:
         ioloop.IOLoop.instance().start()
     except KeyboardInterrupt:
