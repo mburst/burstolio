2334941ec363ad8b1e9555f4f68ec80abae41547
Max Burstein <joesmoe@teamtol.com>
2012-03-10 15:55:49 -0500

    Attepmpting tornado static files

--- tornadows.py
+++ tornadows.py
@@ -9,7 +9,7 @@ parser.add_option('--port', dest='port')
 
 options, args = parser.parse_args()
 
-def runserver():
+'''def runserver():
     app_dir = os.path.abspath(os.path.dirname(__file__))
     sys.path.append(os.path.dirname(app_dir))
     os.environ['DJANGO_SETTINGS_MODULE'] = 'burstolio.settings'
@@ -17,6 +17,24 @@ def runserver():
 
     container = wsgi.WSGIContainer(application)
     server = httpserver.HTTPServer(container)
+    server.listen(options.port)
+    try:
+        ioloop.IOLoop.instance().start()
+    except KeyboardInterrupt:
+        sys.exit(0)'''
+        
+def runserver():
+    app_dir = os.path.abspath(os.path.dirname(__file__))
+    sys.path.append(os.path.dirname(app_dir))
+    os.environ['DJANGO_SETTINGS_MODULE'] = 'burstolio.settings'
+    wsgi_app = tornado.wsgi.WSGIContainer(django.core.handlers.wsgi.WSGIHandler())
+    application = tornado.web.Application([
+        (r"/static/(.*)", web.StaticFileHandler, {"path": "/app/burstolio/static"}),
+        (r".*", FallbackHandler, dict(fallback=wsgi_app),
+    ])
+
+    #container = wsgi.WSGIContainer(application)
+    server = httpserver.HTTPServer(application)
     server.listen(options.port)
     try:
         ioloop.IOLoop.instance().start()
