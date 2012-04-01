e80318332d6696c87be3d997795c2e4b786df1af
Max Burstein <joesmoe@teamtol.com>
2012-04-01 18:44:51 -0400

    Removed URL tracking and added a forward slash to blog posts

--- core/urls.py
+++ core/urls.py
@@ -4,7 +4,7 @@ from django.views.generic import TemplateView
 urlpatterns = patterns('core.views',
     url(r'^$', 'blog', name='home'),
     url(r'^blog/$', 'blog', name='blog'),
-    url(r'^blog/(?P<slug>.*)$', 'entry', name='entry'),
+    url(r'^blog/(?P<slug>.*)/$', 'entry', name='entry'),
     url(r'^subscribe/$', 'subscribe', name='subscribe'),
     url(r'^unsubscribe/$', 'unsubscribe', name='unsubscribe'),
     url(r'^contact/$', 'contact', name='contact'),
