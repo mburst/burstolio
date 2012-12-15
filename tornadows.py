50250b7afa159640563c2676f783f33372702550
Max Burstein <joesmoe@teamtol.com>
2012-12-15 12:53:12 -0500

    Removed dummy text for easier reading

--- tornadows.py
+++ tornadows.py
@@ -8,20 +8,6 @@ parser = OptionParser()
 parser.add_option('--port', dest='port')
 
 options, args = parser.parse_args()
-
-'''def runserver():
-    app_dir = os.path.abspath(os.path.dirname(__file__))
-    sys.path.append(os.path.dirname(app_dir))
-    os.environ['DJANGO_SETTINGS_MODULE'] = 'burstolio.settings'
-    application = django.core.handlers.wsgi.WSGIHandler()
-
-    container = wsgi.WSGIContainer(application)
-    server = httpserver.HTTPServer(container)
-    server.listen(options.port)
-    try:
-        ioloop.IOLoop.instance().start()
-    except KeyboardInterrupt:
-        sys.exit(0)'''
         
 def runserver():
     app_dir = os.path.abspath(os.path.dirname(__file__))
