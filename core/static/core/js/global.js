b58c09b5923957536002ed28e0f4be955dafb428
Max Burstein <mburst@maxburstein.com>
2016-04-06 22:59:26 -0700

    Better spam prevention and upgrading to latest 1.5.x Django release

--- core/static/core/js/global.js
+++ core/static/core/js/global.js
@@ -20,5 +20,9 @@ $(document).ready(function(){
         $(this).remove();
     });
     
+    $("#postcomment").on('submit', function(e){
+        $(this).find("#id_password").val('max');
+    });
+    
     !function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="//platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");
 });
